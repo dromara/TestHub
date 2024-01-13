cb960366cd2f64f37025c5497ba94785e6893437
崔胜利 <cuishengli@croot.com>
2024-01-13 16:18:46 +0800

    构建脚本

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -3,7 +3,7 @@
 # 读取用户输入
 
 read -p "版本号: " DOCKER_IMAGE_TAG
-# read -s -p "Docker Hub 密码: " DOCKER_PASSWORD
+read -s -p "Docker Hub 密码: " DOCKER_PASSWORD
 
 
 # 设置变量
@@ -28,14 +28,14 @@ docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
 # 添加标签
 docker tag $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG $DOCKERHUB_REPO/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
 
-# # 登录到 Docker Hub
-# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# 登录到 Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# # 推送镜像到 Docker Hub
-# docker push $DOCKERHUB_REPO/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+# 推送镜像到 Docker Hub
+docker push $DOCKERHUB_REPO/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
 
-# 注销 Docker Hub
-# docker logout
+注销 Docker Hub
+docker logout
 
 rm -rf ./frontend/*
 rm ./backend/TestHub.jar
--- electron-egg/build.sh
+++ electron-egg/build.sh
@@ -6,7 +6,7 @@ read -p "版本号: " DOCKER_IMAGE_TAG
 
 # 拷贝jar包
 cp ../server/testhub-server/target/testhub-server-$DOCKER_IMAGE_TAG-SNAPSHOT.jar ./build/extraResources/TestHub.jar
-cp ./docker/backend/application.yml ./build/extraResources/application.yml
+cp ../docker/backend/application.yml ./build/extraResources/application.yml
 
 echo "请选择平台:"
 echo "1. win64"
